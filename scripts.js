40b181cdc7e346caed3519554476ab00a2303b7f
Michael Mason <michael@michaeljmason.com>
2017-04-16 20:18:44 -0700

    init repo

---
+++ scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $("#gallery").lightGallery();
+
+
+      AOS.init({
+      offset: 200,
+      duration: 600,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
+
+});
