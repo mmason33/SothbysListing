bf67e0f9b2f4d3ba0d955dafeb4b4386ea30a78e
Michael Mason <michael@michaeljmason.com>
2017-04-23 15:00:49 -0700

    project finished pending merge later

--- scripts.js
+++ scripts.js
@@ -2,11 +2,15 @@ $(document).ready(function() {
   $("#gallery").lightGallery();
 
 
-      AOS.init({
+    AOS.init({
       offset: 200,
       duration: 600,
       easing: 'ease-in-sine',
       delay: 100,
+    });
+
+    $(window).scroll(function(){
+      $(".hero").css("opacity", 1 - $(window).scrollTop() / 250);
     });
 
 });
